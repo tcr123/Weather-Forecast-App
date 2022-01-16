bb1cab87e911da59391a12cbe1254479eec7d087
TAN CHUN RONG <64246294+tcr123@users.noreply.github.com>
2022-01-16 23:07:13 +0800

    Add files via upload

--- R/map.R
+++ R/map.R
@@ -7,12 +7,12 @@ leaflet() %>%
 city_info <- read.csv("weather.csv")
 
 weatherIcons <- iconList(
-  Clear= makeIcon(iconUrl="ImageWeather.png",iconWidth=20,iconHeight=20),
-  Clear= makeIcon(iconUrl="ImageWeather.png",iconWidth=20,iconHeight=20),
-  Rainy= makeIcon(iconUrl="ImageWeather.png", iconWidth=20,iconHeight=20)
+  Clear= makeIcon(iconUrl="sun.png",iconWidth=20,iconHeight=20),
+  Clouds= makeIcon(iconUrl="cloudy.png",iconWidth=20,iconHeight=20),
+  Thunderstorm = makeIcon(iconUrl="lightningRain", iconWidth=20, iconHeight = 20),
+  Rain= makeIcon(iconUrl="rainy.png", iconWidth=20,iconHeight=20)
 )
 
-weatherIcons
 
 leaflet() %>%
   addProviderTiles(providers$Esri.WorldTerrain, group = "World Imagery") %>%
@@ -22,5 +22,13 @@ leaflet() %>%
   setMaxBounds(lng1 = 92, lat1 = -15, lng2 = 122, lat2 = 15) %>%
   addMarkers(data = city_info,
              lng = ~Longitude, 
-             lat = ~Latitude)
+             lat = ~Latitude,
+             icon = ~weatherIcons[city_info$Condition],
+             popup = paste(
+               "<b>",city_info$City,", ", city_info$Country,"</b>","<br>",
+               "<b>Updated: </b>",city_info$DateTime,"<br>",
+               "<b>Weather: </b>",city_info$Weather,"<br>",
+               "<b>Temperature: </b>",city_info$Temperature, " C","<br>",
+               "<b>Wind Speed: </b>",city_info$Wind, " km/h",
+               sep=""))
 
